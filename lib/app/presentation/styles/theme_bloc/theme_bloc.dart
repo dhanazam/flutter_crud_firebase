6d1ef7aba8622a1ae471a1bb828279a157d04e82
Ramadhan <ramadanazam93@gmail.com>
2024-07-15 10:19:14 +0800

    add theme bloc

---
+++ lib/app/presentation/styles/theme_bloc/theme_bloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_crud_firebase/app/services/services.dart';
+
+part 'theme_event.dart';
+part 'theme_state.dart';
+
+class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
+  final SharedPreferencesManager _sharedPreferencesManager =
+      SharedPreferencesManager();
+  ThemeBloc() : super(const ThemeState(isDarkTheme: false)) {
+    on<ThemeInitialEvent>(_onThemeInitialEvent);
+    on<ChangeThemeEvent>(changeThemeEventHandler);
+  }
+
+  FutureOr<void> changeThemeEventHandler(
+      ChangeThemeEvent event, Emitter<ThemeState> emit) {
+    if (state.isDarkTheme) {
+      emit(state.copyWith(isDarkTheme: false));
+      _sharedPreferencesManager.putBool('isDark', false);
+    } else {
+      emit(state.copyWith(isDarkTheme: true));
+      _sharedPreferencesManager.putBool('isDark', true);
+    }
+  }
+
+  Future<void> _onThemeInitialEvent(
+      ThemeInitialEvent event, Emitter<ThemeState> emit) async {
+    final isDarkTheme = await (_sharedPreferencesManager.getBool('isDark'));
+    emit(state.copyWith(isDarkTheme: isDarkTheme));
+  }
+}
