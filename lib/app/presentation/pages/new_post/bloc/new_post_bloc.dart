e832d2f9e74901be89a95267055812fb37ee7fc9
Ramadhan <ramadanazam93@gmail.com>
2024-07-20 22:46:42 +0800

    add new post bloc

---
+++ lib/app/presentation/pages/new_post/bloc/new_post_bloc.dart
@@ -0,0 +1,186 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_crud_firebase/app/core/core.dart';
+import 'package:formz/formz.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io' as io;
+
+part 'new_post_event.dart';
+part 'new_post_state.dart';
+
+class NewPostBloc extends Bloc<NewPostEvent, NewPostState> {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final AuthRepository _authRepository = AuthRepository();
+  final PostRepository _postRepository = PostRepository();
+  NewPostBloc() : super(const NewPostState()) {
+    on<NewPostInitialEvent>(_onNewPostInitialEvent);
+
+    on<OnPostImagePickerEvent>(_onPostImagePickerEvent);
+
+    on<PostTitleChanged>(_onTitleChanged);
+
+    on<PostTitleUnfocused>(_onTitleUnfocused);
+
+    on<PostDescriptionChanged>(_onDescriptionChanged);
+
+    on<PostDescriptionUnfocused>(_onDescriptionUnfocused);
+
+    on<PostFormSubmitted>(_onPostFormSubmitted);
+  }
+
+  Future<void> _onNewPostInitialEvent(
+      NewPostInitialEvent event, Emitter<NewPostState> emit) async {
+    if (event.action == 'update') {
+      emit(
+        state.copyWith(
+          title: PostTitleField.pure(event.postModel.title!),
+          description: PostDescriptionField.pure(event.postModel.description!),
+          cover: PostCover.pure(event.postModel.cover!),
+          isValid: Formz.validate([
+            state.title,
+            state.description,
+            state.cover,
+          ]),
+        ),
+      );
+    }
+  }
+
+  Future<void> _onPostImagePickerEvent(
+      OnPostImagePickerEvent event, Emitter<NewPostState> emit) async {
+    try {
+      final pickedFile = await ImagePicker().pickImage(
+          source: event.kind == 'gallery'
+              ? ImageSource.gallery
+              : ImageSource.camera,
+          imageQuality: 25);
+
+      if (pickedFile != null) {
+        UploadTask uploadTask;
+        Reference ref = _storage.ref().child('images/').child(pickedFile.name);
+
+        uploadTask = ref.putFile(io.File(pickedFile.path));
+        await uploadTask.whenComplete(() async {
+          await ref.getDownloadURL().then((value) {
+            final cover = PostCover.dirty(value);
+            emit(
+              state.copyWith(
+                cover: cover.isValid ? cover : PostCover.pure(value),
+                isValid:
+                    Formz.validate([cover, state.title, state.description]),
+              ),
+            );
+          });
+        });
+      }
+    } catch (e) {
+      emit(state.copyWith(
+          status: AddPostStatus.failure, toastMessage: e.toString()));
+    }
+  }
+
+  Future<void> _onTitleChanged(
+      PostTitleChanged event, Emitter<NewPostState> emit) async {
+    final title = PostTitleField.dirty(event.title);
+    emit(
+      state.copyWith(
+        title: title.isValid ? title : PostTitleField.pure(event.title),
+        status: AddPostStatus.initial,
+        isValid: Formz.validate([title, state.cover, state.description]),
+      ),
+    );
+  }
+
+  Future<void> _onTitleUnfocused(
+      PostTitleUnfocused event, Emitter<NewPostState> emit) async {
+    final title = PostTitleField.dirty(state.title.value);
+    emit(
+      state.copyWith(
+        title: title,
+        status: AddPostStatus.initial,
+        isValid: Formz.validate([title, state.cover, state.description]),
+      ),
+    );
+  }
+
+  Future<void> _onDescriptionChanged(
+      PostDescriptionChanged event, Emitter<NewPostState> emit) async {
+    final description = PostDescriptionField.dirty(event.description);
+    emit(
+      state.copyWith(
+        description: description.isValid
+            ? description
+            : PostDescriptionField.pure(event.description),
+        status: AddPostStatus.initial,
+        isValid: Formz.validate([description, state.cover, state.title]),
+      ),
+    );
+  }
+
+  Future<void> _onDescriptionUnfocused(
+      PostDescriptionUnfocused event, Emitter<NewPostState> emit) async {
+    final description = PostDescriptionField.dirty(state.description.value);
+    emit(
+      state.copyWith(
+        description: description,
+        status: AddPostStatus.initial,
+        isValid: Formz.validate([description, state.cover, state.title]),
+      ),
+    );
+  }
+
+  Future<void> _onPostFormSubmitted(
+      PostFormSubmitted event, Emitter<NewPostState> emit) async {
+    final title = PostTitleField.dirty(state.title.value);
+    final description = PostDescriptionField.dirty(state.description.value);
+    final cover = PostCover.dirty(state.cover.value);
+    emit(
+      state.copyWith(
+        title: title,
+        description: description,
+        cover: cover,
+        status: AddPostStatus.initial,
+        isValid: Formz.validate([
+          title,
+          description,
+          cover,
+        ]),
+      ),
+    );
+    if (state.isValid) {
+      UserModel user = await _authRepository.retrieveCurrentUser().first;
+      if (user.uid != "uid") {
+        if (event.action == 'create') {
+          var param = {
+            "cover": state.cover.value.toString(),
+            "title": state.title.value.toString(),
+            "description": state.description.value.toString(),
+            "userId": user.uid,
+            "timestamp": DateTime.timestamp(),
+            "status": 1
+          };
+          await _postRepository.addPost(param);
+          emit(state.copyWith(
+            status: AddPostStatus.success,
+            toastMessage: 'Post Saved',
+          ));
+        } else {
+          emit(state.copyWith(status: AddPostStatus.submitting));
+          var param = {
+            "cover": state.cover.value.toString(),
+            "title": state.title.value.toString(),
+            "description": state.description.value.toString(),
+            "userId": user.uid,
+          };
+          await _postRepository.updatePost(event.postModel.id!, param);
+          emit(state.copyWith(
+            status: AddPostStatus.success,
+            toastMessage: 'Post Updated',
+          ));
+        }
+      } else {
+        emit(state.copyWith(status: AddPostStatus.failure));
+      }
+    }
+  }
+}
---
+++ lib/app/presentation/pages/new_post/bloc/new_post_state.dart
@@ -0,0 +1,57 @@
+part of 'new_post_bloc.dart';
+
+enum AddPostStatus {
+  initial,
+  loading,
+  success,
+  failure,
+  uploadingImage,
+  submitting,
+  uploadedImage
+}
+
+extension AddPostStatusX on AddPostStatus {
+  bool get isInitial => this == AddPostStatus.initial;
+  bool get isLoading => this == AddPostStatus.loading;
+  bool get isSuccess => this == AddPostStatus.success;
+  bool get isFailure => this == AddPostStatus.failure;
+  bool get isUploadingImage => this == AddPostStatus.uploadingImage;
+  bool get isSubmitting => this == AddPostStatus.submitting;
+  bool get isUploadedImage => this == AddPostStatus.uploadedImage;
+}
+
+final class NewPostState {
+  final AddPostStatus status;
+  final PostCover cover;
+  final PostTitleField title;
+  final PostDescriptionField description;
+  final bool isValid;
+  final String toastMessage;
+
+  const NewPostState({
+    this.status = AddPostStatus.initial,
+    this.cover = const PostCover.pure(),
+    this.title = const PostTitleField.pure(),
+    this.description = const PostDescriptionField.pure(),
+    this.isValid = false,
+    this.toastMessage = '',
+  });
+
+  NewPostState copyWith({
+    AddPostStatus? status,
+    PostCover? cover,
+    PostTitleField? title,
+    PostDescriptionField? description,
+    bool? isValid,
+    String? toastMessage,
+  }) {
+    return NewPostState(
+      status: status ?? this.status,
+      cover: cover ?? this.cover,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isValid: isValid ?? this.isValid,
+      toastMessage: toastMessage ?? this.toastMessage,
+    );
+  }
+}
