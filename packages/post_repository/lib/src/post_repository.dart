fc962ecf0e933e6dc134e7e5e84d880bab620458
dhanazam <ramadanazam93@gmail.com>
2024-07-26 15:08:06 +0800

    create post repo package

---
+++ packages/post_repository/lib/src/post_repository.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_pref/shared_pref.dart';
+import 'package:post_repository/post_repository.dart';
+
+class PostRepository {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  final SharedPreferencesManager _sharedPreferencesManager =
+      SharedPreferencesManager();
+
+  Future<void> addPost(dynamic postModel) async {
+    await _db.collection("posts").add(postModel);
+  }
+
+  Future<void> updatePost(String id, dynamic postModel) async {
+    await _db.collection("posts").doc(id).update(postModel);
+  }
+
+  Future<void> deletePost(String documentId) async {
+    await _db.collection("posts").doc(documentId).delete();
+  }
+
+  Future<List<Post>> retrieveMyPost(String uid) async {
+    QuerySnapshot<Map<String, dynamic>> snapshot =
+        await _db.collection("posts").where("userId", isEqualTo: uid).get();
+    return snapshot.docs
+        .map((docSnapshot) => Post.fromDocumentSnapshot(docSnapshot))
+        .toList();
+  }
+
+  Future<String?> getUserId() {
+    return _sharedPreferencesManager.getString('user_id');
+  }
+}
